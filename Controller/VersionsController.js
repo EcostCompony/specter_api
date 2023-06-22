3b1ae2030ed22d5193875c3a3d32cd24c90cf7b4
tebeskakoi <getmanchuckdan1iil@gmail.com>
2023-06-22 16:40:46 +0300

    mandatory version changed to 0.7

--- Controller/VersionsController.js
+++ Controller/VersionsController.js
@@ -7,7 +7,7 @@ module.exports = (req, res, next) => {
 	try {
 		let v = req.query.v
 
-		return !v || v !== '1.0' ? response.sendDetailedError(6, "invalid request", [{ "key": 'v', "value": v ? v : 'required' }], res) : next()
+		return !v || v !== '0.7' ? response.sendDetailedError(6, "invalid request", [{ "key": 'v', "value": v ? v : 'required' }], res) : next()
 	} catch (error) {
 		return response.sendSystemError(error, res)
 	}
